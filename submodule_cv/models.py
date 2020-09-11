2f78f1722a23649792a1e4b80ac6d4d3def3847f
pouya1991 <pouya.ahmadvand@gmail.com>
2020-09-10 22:13:26 -0700

    fixing singularity errors

--- submodule_cv/models.py
+++ submodule_cv/models.py
@@ -77,7 +77,10 @@ class DeepModel(BaseModel):
         if device is not None:
             if len(device)>1 :
                 self.model = torch.nn.DataParallel(model, device_ids=device).cuda()
-            self.model = model.to(f'cuda:{device[0]}')
+            try :
+                self.model = model.to(f'cuda:{device[0]}')
+            except: 
+                self.model = model.to(f'cuda:0')
         else:
             self.model = model.cuda()
 
