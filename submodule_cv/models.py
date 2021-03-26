ead998ee2ffe24c9123ad18a60425e3a68d1fcd7
adarbandsari <a.darbandsari@ece.ubc.ca>
2021-03-26 12:05:55 -0700

    Freeze Unfreeze

--- submodule_cv/models.py
+++ submodule_cv/models.py
@@ -1,9 +1,9 @@
 import torchvision.models as models
-from efficientnet_pytorch import EfficientNet
 import torch.nn as nn
 import torch
 import os
-
+from submodule_cv.deep_models.models import Model
+from efficientnet_pytorch.model import MBConvBlock
 import numpy as np
 import torch
 
@@ -16,7 +16,7 @@ class BaseModel():
         self.config = config
         self.use_weighted_loss = config["use_weighted_loss"]
         self.continue_train = config["continue_train"]
-        self.freeze = True if "freeze" in config and config["freeze"]!=-1 else False
+        self.freeze_ = True if "freeze" in config and config["freeze"]!=-1 else False
         self.trainable_layer_num = config["freeze"] if self.freeze else None
 
     def forward(self):
@@ -43,7 +43,6 @@ class DeepModel(BaseModel):
             n += ['weighted_loss']
         if self.use_antialias:
             n += ['antialias']
-
         return '_'.join(n)
 
     def __init__(self, config, is_eval=False, class_weight=None, device=None):
@@ -57,30 +56,13 @@ class DeepModel(BaseModel):
         print("Config File:")
         print(config)
         self.is_eval = is_eval
-        self.deep_model = self.config["deep_model"]
+        self.deep_model = self.config["model"]["base_model"]
         self.class_weight = class_weight if self.use_weighted_loss else None
-        self.MixUp = True if 'mix_up' in self.config and self.config['mix_up']['mix_up'] else False
+        self.MixUp = True if 'mix_up' in self.config and self.config['mix_up']['use_mix_up'] else False
 
-        # using efficientnet
-        if ("efficientnet") in self.deep_model:
-            model = EfficientNet.from_pretrained(self.deep_model, num_classes=self.config["num_subtypes"])
-
-        else :
-            model = getattr(models, self.deep_model)
-            model = model(**self.config["parameters"])
-            if "feature_extract" in self.config and self.config["feature_extract"]:
-                for param in model.parameters():
-                    param.requires_grad = False
-
-            # Modified the original HACK! part into an if condition to avoid the need to comment/uncomment lines
-            # when using vgg vs resnet/inception
-            if 'vgg' in self.deep_model:
-                model.classifier._modules['6'] = torch.nn.Linear(4096, self.config["num_subtypes"])
-            elif 'mobilenet' in self.deep_model:
-                model.classifier[1] = torch.nn.Linear(in_features=model.classifier[1].in_features, out_features=self.config["num_subtypes"])
-            else:
-                num_features = model.fc.in_features
-                model.fc = torch.nn.Linear(num_features, self.config["num_subtypes"])
+        ###############
+        # UPDATE
+        model = Model(config["model"])
 
         print(model)
         if device is not None:
@@ -104,12 +86,7 @@ class DeepModel(BaseModel):
                 #self.criterion = torch.nn.BCEWithLogitsLoss(reduction='mean')
 
         print("Parameters to learn:")
-        if self.freeze:
-            for param in model.parameters():
-                param.requires_grad = False
-            rev_child = reversed(list(model.children()))
-            self.freeze_layers(rev_child, self.trainable_layer_num)
-
+        if self.freeze_:
             params_to_update = []
             for name,param in model.named_parameters():
                 if param.requires_grad == True:
@@ -135,32 +112,14 @@ class DeepModel(BaseModel):
             self.load_state(config["load_deep_model_id"], device=device)
             self.model = self.model.eval()
 
-
     def forward(self, input_data):
         output = self.model.forward(input_data)
-
         if type(output).__name__ in ['GoogLeNetOutputs', 'InceptionOutputs'] and config["parameters"]["aux_logits"]:
             logits = output.logits
         else:
             logits = output
-
         probs = torch.softmax(logits, dim=1)
         return logits, probs, output
-
-    def freeze_layers(self, rev_child, num):
-        for layer in rev_child:
-            if num > 0:
-                if isinstance(layer, nn.Sequential) or \
-                   isinstance(layer, models.resnet.BasicBlock) or \
-                   isinstance(layer, models.resnet.Bottleneck) or \
-                   isinstance(layer, models.squeezenet.Fire):
-                    num = self.freeze_layers(reversed(list(layer.children())), num)
-                else:
-                    if sum(p.numel() for p in layer.parameters())!=0:
-                        for param in layer.parameters():
-                            param.requires_grad = True
-                        num -= 1
-        return num
 
     def get_loss(self, logits, labels, output=None,
                  labels_mixed=None, lam=None):
@@ -182,10 +141,10 @@ class DeepModel(BaseModel):
 
     def optimize_parameters(self, logits, labels, output=None,
                             labels_mixed=None, lam=None):
+        self.optimizer.zero_grad()
         self.loss = self.get_loss(logits, labels, output, labels_mixed, lam)
         self.loss.backward()
         self.optimizer.step()
-        self.optimizer.zero_grad()
         return self.loss.item()
 
     def get_current_errors(self):
@@ -194,8 +153,8 @@ class DeepModel(BaseModel):
     def scheduler_step(self):
         self.scheduler.step()
 
-    def get_current_lr(self):
-        return self.optimizer.param_groups[0]['lr']
+    def get_current_lr(self, grp_idx=0):
+        return self.optimizer.param_groups[grp_idx]['lr']
 
     def load_state(self, save_path, device=None):
         if device:
@@ -234,3 +193,115 @@ class DeepModel(BaseModel):
             'optimizer': self.optimizer.state_dict()
         }
         torch.save(state, save_path)
+
+    def freeze_except_bn(self):
+        def freeze_all_but_bn(layer):
+            if not isinstance(layer, torch.nn.BatchNorm2d) and not isinstance(layer, torch.nn.BatchNorm1d):
+                if hasattr(layer, 'weight') and layer.weight is not None:
+                    layer.weight.requires_grad_(False)
+                if hasattr(layer, 'bias') and layer.bias is not None:
+                    layer.bias.requires_grad_(False)
+        self.model.apply(freeze_all_but_bn)
+
+    def make_classifier_layer_trainable(self):
+        for param in self.model.classifier.parameters():
+            param.requires_grad = True
+
+    def make_feature_extractor_layer_trainable(self):
+        for param in self.model.feature_extract.parameters():
+            param.requires_grad = True
+
+    def unfreeze_reverse_layers(self, num_trainable_layers):
+        rev_child = reversed(list(self.model.children()))
+        def unfreeze_layers(rev_child, num):
+            for layer in rev_child:
+                if num > 0:
+                    if isinstance(layer, nn.Sequential) or \
+                    isinstance(layer, models.resnet.BasicBlock) or \
+                    isinstance(layer, models.resnet.Bottleneck) or \
+                    isinstance(layer, models.squeezenet.Fire) or \
+                    isinstance(layer, nn.ModuleList) or \
+                    isinstance(layer, MBConvBlock):
+                        num = freeze_layers(reversed(list(layer.children())), num)
+                    else:
+                        flag = False
+                        if not isinstance(layer, torch.nn.BatchNorm2d) and not isinstance(layer, torch.nn.BatchNorm1d):
+                            if hasattr(layer, 'weight') and layer.weight is not None:
+                                layer.weight.requires_grad_(True)
+                                flag = True
+                            if hasattr(layer, 'bias') and layer.bias is not None:
+                                layer.bias.requires_grad_(True)
+                                flag = True
+                            if flag:
+                                num -= 1
+            return num
+        unfreeze_layers(rev_child, num_trainable_layers)
+
+    def find_trainable_parameters(self):
+        params_to_update = {"feature_extract": [], "classifier": []}
+        for name, param in self.model.named_parameters():
+            if param.requires_grad == True:
+                if "feature_extract" in name:
+                    params_to_update["feature_extract"].append(param)
+                elif "classifier" in name:
+                    params_to_update["classifier"].append(param)
+                else:
+                    raise ValueError(f"{name} should be either _feature_extract_ or _classifier_!")
+        return params_to_update
+
+    def update_optimizer_schedular(self, learning_rate, use_scheduler=True, epoch=None, batch_per_epoch=None):
+        lr = {}
+        if isinstance(learning_rate, float):
+            lr["classifier"]      = learning_rate
+            lr["feature_extract"] = learning_rate / 10
+        elif isinstance(learning_rate, list):
+            assert len(learning_rate)==2, f"There should be two numbers in the list"
+            lr["classifier"]      = max(learning_rate)
+            lr["feature_extract"] = min(learning_rate)
+
+        params    = self.find_trainable_parameters()
+        params_lr = []
+        max_lr    = []
+        log       = ""
+        for layer in ["feature_extract", "classifier"]:
+            # Check if it is empty
+            if params[layer]:
+                params_lr.append({"params": params[layer], "lr": lr[layer]})
+                max_lr.append(lr[layer])
+                log += f"-{layer}: lr = {lr[layer]} "
+
+        assert params_lr, f"There is not parameter for optimizer!"
+
+        optimizer = getattr(torch.optim, self.config["optimizer"]["type"])
+        self.optimizer = optimizer(params_lr, **self.config["optimizer"]["parameters"])
+
+        if use_scheduler:
+            scheduler_type  = self.config["scheduler"]["type"] if "scheduler" in self.config else "OneCycleLR"
+            scheduler_param = self.config["scheduler"]["parameters"] if "scheduler" in self.config else {}
+            if scheduler_type=="OneCycleLR":
+                scheduler_param["max_lr"] = max_lr
+                scheduler_param["epochs"] = epoch
+                scheduler_param["steps_per_epoch"] = batch_per_epoch
+                if isinstance(learning_rate, float):
+                    scheduler_param["pct_start"] = 0.99
+            scheduler = getattr(torch.optim.lr_scheduler, scheduler_type)
+            self.scheduler = scheduler(self.optimizer, **scheduler_param)
+        # Log
+        opt_type = self.config["optimizer"]["type"]
+        print(f"\tOptimizer: {opt_type}: {log}")
+        if use_scheduler:
+            print(f"\tScheduler: {scheduler_type}!")
+
+    def unfreeze(self, num_trainable_layers=-1):
+        if num_trainable_layers==-1:
+            self.make_classifier_layer_trainable()
+            self.make_feature_extractor_layer_trainable()
+        else:
+            self.unfreeze_reverse_layers(num_trainable_layers)
+
+    def freeze(self, num_trainable_layers=-1):
+        self.freeze_except_bn()
+        if num_trainable_layers==-1:
+            self.make_classifier_layer_trainable()
+        else:
+            self.unfreeze_reverse_layers(num_trainable_layers)
